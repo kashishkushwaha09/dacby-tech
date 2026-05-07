53f2e854045ac2cda315fea5933883f34b9c372b
Khushboo Kachhi <kashishkushwaha03.com>
2026-05-07 23:10:26 +0530

    organize imports and improve server.js structure

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const storyRoutes = require("./routes/storyRoutes");
 const scrapeRoutes = require("./routes/scrapeRoutes");
@@ -10,21 +10,27 @@ const scrapeStories = require("./services/scrapeStories");
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/stories", storyRoutes);
 
 app.use("/api/scrape", scrapeRoutes);
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(async() => {
-    console.log("MongoDB connected successfully");
-     await scrapeStories();
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Route not found",
     });
-  })
-  .catch((err) => {
-    console.log("MongoDB connection failed");
-    console.log(err);
-  });
+});
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(async () => {
+        console.log("MongoDB connected successfully");
+        await scrapeStories();
+        console.log("Stories scraped successfully");
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed");
+        console.log(err);
+    });
