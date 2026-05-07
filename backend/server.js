e5bc9833bdfd625f81c5c6c752f500c7b8f580c7
Khushboo Kachhi <kashishkushwaha03.com>
2026-05-07 22:14:06 +0530

    add server setup and authentication routes

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed");
+    console.log(err);
+  });
