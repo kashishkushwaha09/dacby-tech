807b98fe0405b8c176b7d026bec058152624e4cf
Khushboo Kachhi <kashishkushwaha03.com>
2026-05-08 02:35:14 +0530

    wrap Home component in ProtectedRoute for access control

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Navbar />
     
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={  <ProtectedRoute><Home /></ProtectedRoute>} />
 
         <Route path="/login" element={<Login />} />
 
